c1dc9902f6ab2637060be0562c873b105e95cb31
heleneshaikh <helene.shaikh@gmail.com>
2016-12-12 17:49:44 +0100

    format code

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,11 +8,13 @@ $(function () {
 
     describe('RSS Feeds', function () {
 
+        /* Make sure that the allFeeds variable has been defined and that it is not empty.  */
         it('are defined', function () {
             expect(allFeeds).toBeDefined(); //var defined
-            expect(allFeeds.length).not.toBe(0); //length
+            expect(allFeeds.length).not.toBe(0); //length of feeds
         });
 
+        /* Ensures that the allFeeds object has a URL defined and that the URL is not empty. */
         it('have a url', function () {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
@@ -20,6 +22,7 @@ $(function () {
             }
         });
 
+        /* Ensures that the allFeeds object has a name defined and that the name is not empty.*/
         it('have a name', function () {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
@@ -31,15 +34,18 @@ $(function () {
 
     describe('The menu', function () {
 
+        /* Ensures the menu element is hidden by default. */
         it('is hidden by default', function () {
             expect(document.getElementsByTagName('body')[0].className).toBe("menu-hidden");
         });
 
+        /* Ensures the menu changes visibility when the menu icon is clicked.  */
         it('changes on click', function () {
             var menuIcon = $(".menu-icon-link");
             menuIcon.trigger("click");
             expect($("body").hasClass("menu-hidden")).toBe(false);
 
+            //hidden after clicking again
             menuIcon.trigger("click");
             expect($("body").hasClass("menu-hidden")).toBe(true);
         });
@@ -47,7 +53,8 @@ $(function () {
 
 
     describe('Initial Entries', function () {
-
+        /* Ensures when the loadFeed function is called and completes its work, there is at least a single .entry
+         element within the .feed container. */
         beforeEach(function (done) {
             loadFeed(0, done);
         });
@@ -59,6 +66,8 @@ $(function () {
 
 
     describe('New Feed Selection', function () {
+
+        /* Ensures when a new feed is loaded by the loadFeed function that the content actually changes.*/
         var beforeResults, afterResults;
 
         beforeEach(function (done) {
